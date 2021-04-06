e190ec473b2ec5e5b1763a6ed9f72972267131f2
Cory Bass <cory.bass.07@gmail.com>
2021-04-06 13:05:49 -0500

    added instructions on coding conventions in csharp

--- conventions/Program.cs
+++ conventions/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace conventions
 {
-    class Program
+    class ClientActivity
+        // Pascal casing for class names "ClientActivity"
     {
+        // Method name is Pascal as well
+        // method arguments camel case "firstNumber"
         static void Main(string[] args)
         {
+            // local variables like itemCount
+
+            // use userControl instead of usrCtr
+
+            // don't use numbers at the start of variable names
+            int 3cars = 6;
+
+            // Correct
+            string myName;
+            int lastNum;
+            bool isSaved;
+
+            // Avoid
+            // String myName;
+            // Int32 lastNum;
+            // Boolean isSaved;
         }
     }
 }
