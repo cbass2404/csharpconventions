a5cd0f7a9922d563eb8d2180f9c9d2467b55b0f8
Cory Bass <cory.bass.07@gmail.com>
2021-04-06 17:28:12 -0500

    finished with c# studies for the night

--- Active/Program.cs
+++ Active/Program.cs
@@ -10,18 +10,7 @@ namespace Active
     {
         static void Main(string[] args)
         {
-            string myString = "15";
-            string mySecondString = "13";
 
-            // WRONG string result = myString + mySecondString;
-
-            // int num1 = Int32.Parse(myString);
-            // int num2 = Int32.Parse(mySecondString);
-            // int resultInt = num1 + num2;
-            int resultInt = Int32.Parse(myString) + Int32.Parse(mySecondString);
-
-            Console.WriteLine(resultInt);
-            Console.Read();
         }
     }
 }
